2952d127f10e241d9fbf26c870ab72a9eac0b45c
douglas waugh <douglas.waugh.e3@gmail.com>
2016-03-28 14:32:53 +0100

    Add Mechanic tests

--- BikeTours/Trip.cs
+++ BikeTours/Trip.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace BikeTours
 {
-    public class Trip
+    public class Trip : Activity
     {
-        public Trip(IEnumerable<Bicycle> bicycles, IEnumerable<Customer> customers, Vehicle vehicle)
+        public Trip(IEnumerable<Transport> bicycles, IEnumerable<Customer> customers, Vehicle vehicle)
         {
             Bicycles = bicycles;
             Customers = customers;
@@ -20,9 +22,14 @@ namespace BikeTours
             }
         }
 
-        public IEnumerable<Bicycle> Bicycles { get; private set; }
+        public IEnumerable<Transport> Bicycles { get; private set; }
         public IEnumerable<Customer> Customers { get; private set; }
         public Vehicle Vehicle { get; private set; }
+    }
+
+    public interface Activity
+    {
+        IEnumerable<Transport> Bicycles { get; }
     }
 
     public class Vehicle
@@ -39,12 +46,36 @@ namespace BikeTours
         }
     }
 
-    public class Bicycle
+    public class Bicycle : Transport
     {
-        public void FixBrakes(Mechanic mechanic)
+        private Brakes _brakes;
+        private readonly Velocity _velocity;
+
+        public Bicycle(Brakes brakes, Velocity velocity)
         {
-            
+            _brakes = brakes;
+            _velocity = velocity;
         }
+
+        public void FixBrakes(Brakes brakes)
+        {
+            _brakes = brakes;
+        }
+
+        public void ApplyBrakes()
+        {
+            _brakes.Apply(_velocity);
+        }
+    }
+
+    public interface Transport
+    {
+        void FixBrakes(Brakes brakes);
+    }
+
+    public interface Velocity
+    {
+        void Slow();
     }
 
     public interface Preparer
@@ -56,10 +87,90 @@ namespace BikeTours
     public class MechanicTests
     {
         [Test]
-        public void This_is_going_to_be_a_test()
+        public void Testing_concreate_instances()
         {
+            var oldBrakes = new Brakes(0);
+            var velocity = new MilesPerHour(1);
+            var bicycle = new Bicycle(oldBrakes, velocity);
+            var trip = new Trip(new[] {bicycle}, null, null);
 
+            var mechanic = new Mechanic();
+
+            mechanic.PrepareTrip(trip);
+
+            Assert.DoesNotThrow(bicycle.ApplyBrakes);
         }
+
+        [Test]
+        public void Testing_the_mechanic_using_the_transport_interface_for_bicycle()
+        {
+            var transport = Substitute.For<Transport>();
+            var trip = new Trip(new[] {transport}, null, null);
+
+            var mechanic = new Mechanic();
+
+            mechanic.PrepareTrip(trip);
+
+            transport.Received().FixBrakes(Arg.Any<Brakes>());
+        }
+
+        [Test]
+        public void Testing_the_mechanic_using_the_activity_and_transport_interfaces_for_trip_and_bicycle()
+        {
+            var transport = Substitute.For<Transport>();
+            var trip = Substitute.For<Activity>();
+            trip.Bicycles.Returns(new[] {transport});
+
+            var mechanic = new Mechanic();
+
+            mechanic.PrepareTrip(trip);
+
+            transport.Received().FixBrakes(Arg.Any<Brakes>());
+        }
+    }
+
+    public class MilesPerHour : Velocity
+    {
+        private int _milesPerHour;
+
+        public MilesPerHour(int milesPerHour)
+        {
+            _milesPerHour = milesPerHour;
+        }
+
+        public void Slow()
+        {
+            if (_milesPerHour > 0)
+            {
+                _milesPerHour -= 1;
+            }
+        }
+    }
+
+    public class Brakes
+    {
+        private int _brakeApplications;
+
+        public Brakes(int brakeApplications)
+        {
+            _brakeApplications = brakeApplications;
+        }
+
+        public void Apply(Velocity transport)
+        {
+            if (_brakeApplications > 0)
+            {
+                _brakeApplications -= 1;
+                transport.Slow();
+                return;
+            }
+
+            throw new BrakesFailureException();
+        }
+    }
+
+    public class BrakesFailureException : Exception
+    {
     }
 
     public class Mechanic : Preparer
@@ -72,7 +183,7 @@ namespace BikeTours
             }
         }
 
-        private void PrepareBicycle(Bicycle bicycle)
+        private void PrepareBicycle(Transport bicycle)
         {
             CleanBicycle(bicycle);
             PumpTires(bicycle);
@@ -80,16 +191,16 @@ namespace BikeTours
             CheckBrakes(bicycle);
         }
 
-        private void CheckBrakes(Bicycle bicycle)
+        private void CheckBrakes(Transport bicycle)
         {
-            bicycle.FixBrakes(this);
+            bicycle.FixBrakes(new Brakes(100));
         }
 
-        private void LubeChain(Bicycle bicycle) { }
+        private void LubeChain(Transport bicycle) { }
 
-        private void PumpTires(Bicycle bicycle) { }
+        private void PumpTires(Transport bicycle) { }
 
-        private void CleanBicycle(Bicycle bicycle) { }
+        private void CleanBicycle(Transport bicycle) { }
     }
 
     public class TripCoordinator : Preparer
