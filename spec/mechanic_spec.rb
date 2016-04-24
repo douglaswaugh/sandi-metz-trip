ba5ac445e2c3d3cd72f704ade7dd1376da198cc6
douglas waugh <douglas.waugh.e3@gmail.com>
2016-04-24 14:33:30 +0100

    Add test for concrete Mechanic et al

--- spec/mechanic_spec.rb
+++ spec/mechanic_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
+class Brakes
+end
+
+class MilesPerHour
+end
+
+class Bicycle
+end
+
 describe Mechanic do
 	before :each do
 		@mechanic = Mechanic.new
@@ -8,6 +17,21 @@ describe Mechanic do
 	describe "new" do
 		it "should return new Mechanic" do
 			expect(@mechanic).to be_a Mechanic
+		end
+	end
+
+	describe "prepare" do
+		it "should be able to test using concrete instances" do
+			var oldBrakes = new Brakes(0);
+            var velocity = new MilesPerHour(1);
+            var bicycle = new Bicycle(oldBrakes, velocity);
+            var trip = new Trip(new[] {bicycle}, null, null);
+
+            var mechanic = new Mechanic();
+
+            mechanic.PrepareTrip(trip);
+
+            Assert.DoesNotThrow(bicycle.ApplyBrakes);
 		end
 	end
 end
